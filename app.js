4fefff8d5ae18055e5f24f125b9f4e42d50c990b
William Johnson <wjohnson1000@gmail.com>
2016-01-10 17:20:01 -0700

    resolved conflicts with auth

--- app.js
+++ app.js
@@ -4,16 +4,83 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var api = require('./api');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var session = require('express-session')
+
 var routes = require('./routes/index');
+var users = require('./routes/users');
+var auth = require('./routes/auth')
 
 var app = express();
 
+passport.use(new GoogleStrategy({
+    clientID: '124221571735-b5t8li62qid2pqkk3a714vfkvr1a5k7k.apps.googleusercontent.com',
+    clientSecret: 's4-7rvZmWWRrLtGCh7Tkd_3E',
+    callbackURL: "https://trailmix.firebaseapp.com/views/timeline.html"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+      return done(null, profile);
+    });
+  }
+));
+
+passport.serializeUser(function(user,done){
+  done(null, user)
+})
+
+passport.deserializeUser(function(obj,done){
+  done(null, obj)
+})
+
+// view engine setup
+app.use(express.static('./views'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboardcat',
+  resave: true,
+  saveUninitialized: true,
+}))
 
+// user authenication
+app.use(passport.initialize());
+app.use(passport.session());
+
+// run authentication
+function ensureAuthenticated(req, res, next) {
+ console.log("ensureAuthenticated",req.isAuthenticated());
+ if (req.isAuthenticated()) { return next(); }
+ res.redirect('/');
+}
+
+// google oauth
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/plus.login' }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('https://trailmix.firebaseapp.com/views/timeline.html');
+  });
+
+app.get('/logout', function(req, res, next){
+  req.logout();
+  res.redirect('/')
+})
+
+// Routes
 app.use('/', routes);
+app.use('/users', ensureAuthenticated, users);
 
 app.get('/read', function(request, response){
   api.posts.readAll(response);
@@ -54,8 +121,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000, function(){
-  console.log('listening');
-});
+app.listen(8080, function(){
+  console.log('locked into 8080')
+})
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('sending user to splash page')
-  res.redirect('https://trailmix.firebaseapp.com/index.html');
+  res.redirect('https://trailmix.firebaseapp.com/views/timeline.html')
 });
 
 module.exports = router;
