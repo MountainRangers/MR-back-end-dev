989baa18618cf64e6a7795d1edffec3b4c2d16d9
davidtadams <adams.t.david@gmail.com>
2016-01-11 13:27:42 -0700

    adding routes for navigation

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('landing', {title: 'TrailMix'});
 });
 
+router.get('/editor', function(req, res, next) {
+  res.render('editor', {title: 'TrailMix'});
+});
+
+router.get('/makeprofile', function(req, res, next) {
+  res.render('makeprofile', {title: 'TrailMix'});
+});
+
+router.get('/post', function(req, res, next) {
+  res.render('post', {title: 'TrailMix'});
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', {title: 'TrailMix'});
+});
+
+router.get('/settings', function(req, res, next) {
+  res.render('settings', {title: 'TrailMix'});
+});
+
+router.get('/timeline', function(req, res, next) {
+  res.render('timeline', {title: 'TrailMix'});
+});
+
 module.exports = router;
