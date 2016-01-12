13cac7544abd2b2a258d1c2f6672fb04e8763f11
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-12 15:15:26 -0700

    adding more styling

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,21 +1,24 @@
-$(document).ready(function(){
-  $('.google-button').mouseover(function(){
-    $('.shape-one').css('stroke', 'white')
-    $('.shape-two').css('stroke', 'white')
-  })
-})
+// $(document).ready(function(){
+//   $('.google-button').mouseover(function(){
+//     $('.shape-one').css('fill', 'white')
+//   })
+// })
 
+
+//clock
 $(document).ready(function(){
 	setInterval(function(){
 		var now = new Date();
 		var hours = now.getHours();
 		var minutes = now.getMinutes();
+
 		if (hours < 10) {
 			hours = "0" + hours;
 		}
 		if (minutes < 10) {
 			minutes = "0" + minutes;
 		}
+
 		if (hours > 12){
 			var hours = hours - 12;
 			var ap = "p";
@@ -27,35 +30,48 @@ $(document).ready(function(){
 		}else {
 			var ap = "a";
 		}
-	}, 200);
+		document.getElementById('clock').innerHTML = hours + ":" + minutes + ap;
+	}, 1000);
 });
+
 $(".cont").ready(function(){
 	setInterval(function(){
+		var height = $(".foreground").height();
+		var top = $(".foreground").height();
+		var div = $(window).width();
+		$(".cont").css("height", height + 30);
+		$(".foreground").css("top", -top+15);
+		$("#clock").css("margin-top", - top - div/2.3);
+		$(".text").css("margin-top", top - 60);
 	}, 0.01);
 });
+
 //color settings
 $(document).ready(function(){
 	setInterval(function(){
 		var dawn = '#78d';
 		var day = '#1bf';
 		var dusk = '#036';
+
 		var mood = new Date();
 		var second = mood.getSeconds();
-		if(second < 5){
+		if(second < 20){
 			$('body').css('background', dawn);
-			$('.shape-one').css('fill', dawn);
+			$('.trees').css('fill', dawn);
 			$('.sky-dawn').css('opacity','1');
 			$('.sky-day').css('opacity','1');
-		} else if(second < 10) {
+			$('.hills').css('fill','#c66');
+		} else if(second < 40) {
 			$('body').css('background', day);
-			$('.shape-one').css('fill', day);
-			$('.sky-dawn').css('opacity','1');
+			$('.trees').css('fill', day);
+			$('.sky-dawn').css('opacity','0');
 			$('.sky-day').css('opacity','1');
+			$('.hills').css('fill','#db9');
 		} else {
-			$('body').css('background', dawn);
-			$('.shape-one').css('fill', dawn);
-      $('.sky-dawn').css('opacity','1');
-			$('.sky-day').css('opacity','1');
+			$('body').css('background', dusk);
+			$('.trees').css('fill', dusk);
+			$('.sky-day').css('opacity','0');
+			$('.hills').css('fill','#0be');
 		}
 	});
 });
