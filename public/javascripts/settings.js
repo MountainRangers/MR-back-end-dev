e1505c3139ce799d384e8ff6de9909729ffa221a
William Johnson <wjohnson1000@gmail.com>
2016-01-12 09:33:48 -0700

    hide edit text field when tap anywhere on else on screen to cancel edit

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -3,7 +3,13 @@ $changename = $('.change-name');
 $changeemail = $('.change-email');
 $changesocialmedia = $('.change-social-media');
 $changepersonalinfo = $('.change-personal-info');
-$submit = $('.edit settings h3');
+$main = $('main');
+//$submit = $('.edit settings h3');
+
+
+$main.on('click', function(){
+  tapToCancel($main);
+});
 
 $changename.on('click', function(){
   editInfo($changename);
@@ -24,11 +30,25 @@ $changepersonalinfo.on('click', function(){
 function editInfo(info){
   info.parents('.edit-settings').find('h2').hide();
   info.parents('.edit-settings').find('.hidden').show();
-  info.parents('.edit-settings').find('h3').text('Submit');
+  var $submitButton = info.parents('.edit-settings').find('h3');
+  $submitButton.text('Submit');
+  submitChange($submitButton);
 }
 
 function submitChange(submit){
   submit.on('click', function(){
     console.log('submitted');
+    //ajax request
+  })
+}
+
+function tapToCancel(main){
+  main.on('click', function(){
+    if($('.hidden').is(':visible')){
+      console.log('hide it');
+      main.find('.hidden').hide();
+      main.find('.edit-settings').find('h2').show();
+      main.find('.edit-settings').find('h3').text('Change');
+    }
   })
 }
