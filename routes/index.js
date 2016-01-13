78647d04bf635987b5c745c7cafe3bf8420c42be
davidtadams <adams.t.david@gmail.com>
2016-01-12 23:09:17 -0700

    adding authentication check to all routes

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,31 @@ var express = require('express');
 var router = express.Router();
 var api = require('./api');
 
-
-
 router.get('/', function(req, res, next) {
+  req.session.destroy();
   res.render('landing', {
     title: 'TrailMix'
   });
 });
 
-router.get('/editor', function(req, res, next) {
+router.get('/editor', ensureAuthenticated, function(req, res, next) {
   res.render('editor', {
     title: 'TrailMix'
   });
 });
 
-router.get('/makeprofile', function(req, res, next) {
-  res.render('makeprofile', {
-    title: 'TrailMix',
-    profile: req.user
-  });
+router.get('/makeprofile', ensureAuthenticated, function(req, res, next) {
+  if (req.user.id) {
+    res.redirect('/timeline');
+  } else {
+    res.render('makeprofile', {
+      title: 'TrailMix',
+      profile: req.user
+    });
+  }
 });
 
-router.post('/makeprofile', function(req, res, next) {
+router.post('/makeprofile', ensureAuthenticated, function(req, res, next) {
   api.users.createUser({
     email: req.user.email,
     username: req.body.userName,
@@ -31,20 +34,17 @@ router.post('/makeprofile', function(req, res, next) {
     photo_url: req.user.profilePhoto,
     personal_info: 'Please add some personal info'
   }).then(function(id) {
-
-
-
-    res.redirect('/timeline/' + id[0]);
+    res.redirect('/timeline');
   });
 });
 
-router.get('/post', function(req, res, next) {
+router.get('/post', ensureAuthenticated, function(req, res, next) {
   res.render('post', {
     title: 'TrailMix'
   });
 });
 
-router.get('/profile/:userid', function(req, res, next) {
+router.get('/profile/:userid', ensureAuthenticated, function(req, res, next) {
   api.users.getUser(req.params.userid).then(function(userdata) {
     var date = formatDate(userdata.created_at);
     res.render('profile', {
@@ -60,7 +60,7 @@ router.get('/profile/:userid', function(req, res, next) {
   });
 });
 
-router.get('/settings/:userid', function(req, res, next) {
+router.get('/settings/:userid', ensureAuthenticated, function(req, res, next) {
   api.users.getUser(req.params.userid).then(function(userdata) {
     var date = formatDate(userdata.created_at);
     res.render('settings', {
@@ -76,7 +76,7 @@ router.get('/settings/:userid', function(req, res, next) {
   });
 });
 
-router.get('/timeline/:userid', function(req, res, next) {
+router.get('/timeline', ensureAuthenticated, function(req, res, next) {
   api.posts.readAll().then(function(posts) {
     res.render('timeline', {
       post: posts,
@@ -94,6 +94,11 @@ function formatDate(dateString){
   return formattedDate;
 }
 
-
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+ res.redirect('/');
+}
 
 module.exports = router;
