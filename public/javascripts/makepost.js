25c08dfd7d0f2dc4c79a3e75db70f0c0f5449b2f
William Johnson <wjohnson1000@gmail.com>
2016-01-14 12:28:43 -0700

    add post route fully functional

--- public/javascripts/makepost.js
+++ public/javascripts/makepost.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
-  $('.display-name').hide();
-  $('display-name').show(500);
-  var $postForm = $('.create-post-form');
-  var $hikeTitle = $('.title-input');
-  var $hikeBody = $('.body-input');
-  var $submitButton = $('.submit-button');
-  $submitButton.on('click', function(event) {
-    event.preventDefault();
-    var $postTag = $('input[type="radio"]:checked').val();
-    console.log($postTag);
-    var postObject = {};
-    $.ajax({
-      url: '/addpost',
-      method: 'POST',
-      data: {
-        title: $hikeTitle.val(),
-        body: $hikeBody.val(),
-        tag: $postTag
-      },
-      success: function(data) {
-        console.log('success');
-        // console.log($postTag);
-        location.pathname = '/timeline';
-      },
-      error: function() {
-        console.error('failed to make post');
-      }
+//  $('.display-name').hide();
+//  $('display-name').show(500);
+//  if(navigator.geolocation){
+//    console.log('navigation supported by browser');
+//  }
+    navigator.geolocation.getCurrentPosition(function(position){
     });
+  
+  $('.submit-button').on('click', function(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(sendPost)
   });
-})
+});
+
+function sendPost(position){
+  $.ajax({
+    url: '/addpost',
+    method: 'POST',
+    data: {
+      title: $('.title-input').val(),
+      body: $('.body-input').val(),
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      tag: $('input[type="radio"]:checked').val()
+    },
+    success: function() {
+      location.pathname = '/timeline';
+    },
+    error: function() {
+      console.error('failed to make post');
+    }
+  });
+}
