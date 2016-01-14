91e351a716ab0ccc25361e4743a7b00384acf1f0
davidtadams <adams.t.david@gmail.com>
2016-01-13 17:54:29 -0700

    adding filter functionality for timeline view

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -4,7 +4,31 @@ if (window.location.href[window.location.href.length - 1] === '#') {
 
 $(document).ready(function(){
   $('.down-arrow').on('click', function(){
-    $('.drop-down').toggleClass('hidden')
+    $('.drop-down').toggleClass('hidden');
+  })
+
+  $('.tl-tag').on('click', function(event) {
+    filterTags(event.currentTarget.dataset.tag);
+  })
+
+  $('.tag-icon').on('click', function(event) {
+    filterTags(event.currentTarget.dataset.tag);
+    $('.drop-down').toggleClass('hidden');
   })
 
 })
+
+function filterTags(tag) {
+  var $posts = $('.post');
+  for (var i = 0; i < $posts.length; i++) {
+    if (tag === 'all') {
+      $posts[i].style.display = '';
+    } else {
+      if ($posts[i].dataset.tag != tag) {
+        $posts[i].style.display = 'none';
+      } else {
+        $posts[i].style.display = '';
+      }
+    }
+  }
+}
